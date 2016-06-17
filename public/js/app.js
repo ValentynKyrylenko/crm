8b717f67bc6d04711434686045e82baad0d2e7f7
Valentyn <valentynmail@gmail.com>
2016-06-17 17:23:55 +0300

    xxx

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('crmApp', ['ui.router','ngResource','ngDialog', 'angularUtils.directives.dirPagination','chart.js', 'pikaday','ngMessages','angularMoment'])
+angular.module('crmApp', ['ui.router','ngResource','ngDialog', 'angularUtils.directives.dirPagination','chart.js', 'pikaday','angularMoment'])
     .config(['ChartJsProvider', function (ChartJsProvider) {
         // Configure all charts
         ChartJsProvider.setOptions({
